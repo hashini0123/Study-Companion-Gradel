460574faa4914b78a0a8673ec051c4a3955f13d2
hashini0123 <nanayakkarahashini5@gmail.com>
2026-09-06 17:04:46 +0530

    Add getDocumentsByUserId endpoint to DocumentController for retrieving user-specific documents

--- src/main/java/edu/icet/study_companion/controller/DocumentController.java
+++ src/main/java/edu/icet/study_companion/controller/DocumentController.java
@@ -1,15 +1,15 @@
 package edu.icet.study_companion.controller;
 
 import edu.icet.study_companion.dto.DocumentDTO;
+import edu.icet.study_companion.dto.DocumentListItemDTO;
 import edu.icet.study_companion.service.DocumentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +25,12 @@ public class DocumentController {
 
         DocumentDTO response = documentService.uploadDocument(userId, file);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DocumentListItemDTO>> getDocumentsByUSerId(@RequestParam("user_id") Integer userId){
+        List<DocumentListItemDTO> document = documentService.getDocumentsByUserId(userId);
+        return new ResponseEntity<>(document, HttpStatus.OK);
     }
 
 }
